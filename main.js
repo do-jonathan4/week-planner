5090b541e3405bd0709717bd2a3dcababa7222e7
Jonathan Do <jdo697@gmail.com>
2020-10-15 21:33:39 -0700

    saved

---
+++ main.js
@@ -0,0 +1,9 @@
+var day = document.querySelectorAll('h2');
+var schedule = document.querySelector('.event-viewer');
+
+schedule.addEventListener('click', dayOfWeek);
+
+function dayOfWeek() {
+  console.log('h');
+}
+console.log('hi');
