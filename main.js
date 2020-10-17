ad04dea786a48db02a23fee791ab087a5577cae3
Jonathan Do <jdo697@gmail.com>
2020-10-17 04:32:04 -0700

    progress

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var selectDay = document.querySelector('.week');
 var schedule = document.querySelector('.event-viewer');
+var addEntry = document.querySelector('.addEntry');
+var modal = document.querySelector('.modal');
 
 selectDay.addEventListener('click', dayOfWeek);
+addEntry.addEventListener('click', entryModal);
 
 function dayOfWeek(event) {
   if (event.target.className !== 'day') {
@@ -10,3 +13,12 @@ function dayOfWeek(event) {
   var sentence = event.target.textContent;
   schedule.textContent = 'Scheduled Events for ' + sentence;
 }
+
+function entryModal() {
+  modal.classList.remove('hidden');
+}
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.classList.add('hidden');
+  }
+};
