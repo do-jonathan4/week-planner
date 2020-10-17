8488430e60c82dac6f8a1bd8aca2a476aaa34a64
Jonathan Do <jdo697@gmail.com>
2020-10-17 12:09:57 -0700

    completed can add entry

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ var selectDay = document.querySelector('.week');
 var schedule = document.querySelector('.event-viewer');
 var addEntry = document.querySelector('.addEntry');
 var modal = document.querySelector('.modal');
+var entryWeek = document.getElementById('entryWeek');
+var entrySubmit = document.getElementById('entrySubmit');
+var entryTime = document.getElementById('entryTime');
+var entryDescription = document.getElementById('entryDescription');
+var tableRow = document.querySelectorAll('tr');
 
 selectDay.addEventListener('click', dayOfWeek);
 addEntry.addEventListener('click', entryModal);
+entrySubmit.addEventListener('click', addTask);
 
 function dayOfWeek(event) {
   if (event.target.className !== 'day') {
@@ -22,3 +28,17 @@ window.onclick = function (event) {
     modal.classList.add('hidden');
   }
 };
+
+function addTask(event) {
+  for (var i = 1; i < tableRow.length; i++) {
+    if (!tableRow[i].firstElementChild.textContent && !tableRow[i].lastElementChild.textContent) {
+      tableRow[i].firstElementChild.textContent = entryTime.value;
+      tableRow[i].lastElementChild.textContent = entryDescription.value;
+
+      event.preventDefault();
+      modal.classList.add('hidden');
+      entryTime.value = '';
+      entryDescription.value = '';
+    }
+  }
+}
