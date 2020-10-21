f321c85bd67baa7749cdab4851849cc229f87dc4
Jonathan Do <jdo697@gmail.com>
2020-10-21 03:36:18 -0700

    progress

--- main.js
+++ main.js
@@ -1,31 +1,80 @@
+var schedule = {
+  Sunday: {
+    task: []
+  },
+  Monday: {
+    task: []
+  },
+  Tuesday: {
+    task: []
+  },
+  Wednesday: {
+    task: []
+  },
+  Thursday: {
+    task: []
+  },
+  Friday: {
+    task: []
+  },
+  Saturday: {
+    task: []
+  }
+};
+
 var selectDay = document.querySelector('.week');
-var schedule = document.querySelector('.event-viewer');
+var today = document.querySelector('.today');
 var addEntry = document.querySelector('.addEntry');
 var modal = document.querySelector('.modal');
 var entrySubmit = document.getElementById('entrySubmit');
 var entryTime = document.getElementById('entryTime');
 var entryDescription = document.getElementById('entryDescription');
 var tableBody = document.querySelector('tbody');
-var updateButton = document.getElementById('updateButton');
+var updateButton = document.querySelector('table');
+var modalTitle = document.querySelector('.modalTitle');
+var entryWeek = document.getElementById('entryWeek');
 
 selectDay.addEventListener('click', dayOfWeek);
 addEntry.addEventListener('click', entryModal);
 window.addEventListener('click', closeModal);
 entrySubmit.addEventListener('click', addTask);
-updateButton.addEventListener('click', entryModal);
+updateButton.addEventListener('click', updateTask);
 
 function dayOfWeek(event) {
-  if (event.target.className !== 'day') {
+  if (event.target.className === 'week') {
     return;
   }
   var sentence = event.target.textContent;
-  schedule.textContent = 'Scheduled Events for ' + sentence;
+  today.textContent = 'Scheduled Events for ' + sentence;
+  viewDay(sentence);
+}
+function viewDay(forToday) {
+  var todaySchedule = schedule[forToday].task;
+  tableBody.innerHTML = '';
+
+  for (var i = 0; i < todaySchedule.length; i++) {
+    var row = document.createElement('tr');
+    var cell1 = document.createElement('td');
+    var cell2 = document.createElement('td');
+    var updateButton = document.createElement('button');
+    updateButton.className =
+      'update' + ' ' +
+      forToday + ' ' +
+      todaySchedule[i].time
+      ;
+    updateButton.textContent = "Update";
+
+    cell1.textContent = todaySchedule[i].time;
+    cell2.textContent = todaySchedule[i].description;
+    cell2.appendChild(updateButton);
+    row.append(cell1, cell2);
+    tableBody.appendChild(row);
+  }
 }
 
 function entryModal() {
   modal.classList.remove('hidden');
 }
-
 function closeModal(event) {
   if (event.target === modal) {
     modal.classList.add('hidden');
@@ -34,20 +83,29 @@ function closeModal(event) {
 
 function addTask(event) {
   event.preventDefault();
-  var row = tableBody.insertRow(0);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
 
-  cell1.textContent = entryTime.value;
-  cell2.textContent = entryDescription.value;
+  var newTask = {};
+  newTask.day = entryWeek.value;
+  newTask.time = entryTime.value;
+  newTask.description = entryDescription.value;
 
-  var update = document.createElement('button');
-  update.textContent = 'Update';
-  update.id = 'updateButton';
-
-  cell2.appendChild(update);
+  schedule[entryWeek.value].task.push(newTask);
 
   modal.classList.add('hidden');
+  modalTitle.textContent = 'Add Entry';
+  entryWeek.value = '';
   entryTime.value = '';
   entryDescription.value = '';
+}
+
+function updateTask(event) {
+  if (event.target.className.indexOf('update') === -1) {
+    return;
+  }
+  entryModal();
+  var updateClass = event.target.classList;
+  modalTitle.textContent = 'Update Entry';
+  entryWeek.value = updateClass[1];
+  entryTime.value = updateClass[2];
+
 }
