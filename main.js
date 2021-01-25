281c7de746bde53270ae404d46a0b2f1c33058d7
Jonathan Do <jdo697@gmail.com>
2021-01-25 05:23:11 -0800

    saved

--- main.js
+++ main.js
@@ -1,147 +1,187 @@
 var schedule = {
-  Sunday: {
-    task: []
+  Sunday: [],
+  Monday: [],
+  Tuesday: [],
+  Wednesday: [],
+  Thursday: [],
+  Friday: [],
+  Saturday: []
+};
+
+var todoList = {
+  addEntry: function(day, newTask) {
+    schedule[day].push(newTask);
   },
-  Monday: {
-    task: []
+  updateEntry: function(day, position, newTask) {
+    schedule[day][position] = newTask;
   },
-  Tuesday: {
-    task: []
+  deleteEntry: function (day, position) {
+    schedule[day].splice(position, 1);
+  }
+};
+var handlers = {
+  addEntry: function() {
+    var modal = document.getElementById('modal-entry');
+    var entryWeek = document.getElementById('entryWeek');
+    var entryTime = document.getElementById('entryTime');
+    var entryDescription = document.getElementById('entryDescription');
+    var entrySubmit = document.getElementById('entrySubmit');
+
+    modal.classList.remove('hidden');
+
+    entrySubmit.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      var newTask = {
+        day: entryWeek.value,
+        time: entryTime.value,
+        description: entryDescription.value,
+      };
+
+      todoList.addEntry(entryWeek.value, newTask);
+      view.displayTodos(entryWeek.value);
+
+      modal.classList.add('hidden');
+      entryWeek.value = '';
+      entryTime.value = '';
+      entryDescription.value = '';
+    });
   },
-  Wednesday: {
-    task: []
+  updateEntry: function(day, position) {
+    var modalUpdate = document.getElementById('modal-update');
+    var updateWeek = document.getElementById('updateWeek');
+    var updateTime = document.getElementById('updateTime');
+    var updateDescription = document.getElementById('updateDescription');
+    var updateSubmit = document.getElementById('updateSubmit');
+
+    modalUpdate.classList.remove('hidden');
+
+    updateWeek.value = schedule[day][position].day;
+    updateTime.value = schedule[day][position].time;
+    updateDescription.value = schedule[day][position].description;
+
+    updateSubmit.addEventListener('click', function(event) {
+      event.preventDefault();
+
+      var newTask = {
+        day: updateWeek.value,
+        time: updateTime.value,
+        description: updateDescription.value,
+      };
+
+      if (day === updateWeek.value) {
+        todoList.updateEntry(updateWeek.value, position, newTask);
+      } else {
+        todoList.addEntry(updateWeek.value, newTask);
+      }
+
+      view.displayTodos(updateWeek.value);
+
+      modalUpdate.classList.add('hidden');
+      updateWeek.value = '';
+      updateTime.value = '';
+      updateDescription.value = '';
+    });
   },
-  Thursday: {
-    task: []
+  deleteEntry: function(day, position) {
+    var modalDelete = document.getElementById('modal-delete');
+    modalDelete.classList.remove('hidden');
+
+    modalDelete.addEventListener('click', function(event) {
+      if (event.target.className === 'delete-yes') {
+        todoList.deleteEntry(day, position);
+        modalDelete.classList.add('hidden');
+        view.displayTodos(day);
+      } else {
+        modalDelete.classList.add('hidden');
+      }
+    });
+  }
+};
+var view = {
+  displayTodos: function(day) {
+    var tableBody = document.querySelector('tbody');
+    var today = document.querySelector('.today');
+
+    today.textContent = 'Scheduled Events for ' + day;
+
+    var todaySchedule = schedule[day];
+    tableBody.innerHTML = '';
+
+    todaySchedule.forEach(function (row, index) {
+      var tr = document.createElement('tr');
+      var tdTime = document.createElement('td');
+      var tdDesc = document.createElement('td');
+      tr.className = index + ' ' + day;
+
+      tdTime.textContent = todaySchedule[index].time;
+      tdDesc.textContent = todaySchedule[index].description;
+
+      tdDesc.append(this.createDeleteButton(), this.createUpdateButton());
+      tr.append(tdTime, tdDesc);
+      tableBody.append(tr);
+    }, this);
   },
-  Friday: {
-    task: []
+  createUpdateButton: function() {
+    var updateButton = document.createElement('button');
+    updateButton.textContent = 'Update';
+    updateButton.className = 'updateButton';
+    return updateButton;
   },
-  Saturday: {
-    task: []
+  createDeleteButton: function() {
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'deleteButton';
+    return deleteButton;
+  },
+  entryListener: function () {
+    window.addEventListener('click', function (event) {
+      var elementClicked = event.target;
+      if (elementClicked.className === 'addEntry') {
+        handlers.addEntry();
+      }
+    });
+  },
+  buttonListener: function() {
+    var tableBody = document.querySelector('tbody');
+
+    tableBody.addEventListener('click', function(event) {
+      var elementClicked = event.target;
+      var position = elementClicked.parentNode.parentNode.classList[0];
+      var day = elementClicked.parentNode.parentNode.classList[1];
+      if (elementClicked.className === 'deleteButton') {
+        handlers.deleteEntry(day, position);
+      }
+      if (elementClicked.className === 'updateButton') {
+        handlers.updateEntry(day, position);
+      }
+    });
+  },
+  modalListener: function() {
+    window.addEventListener('click', function(event) {
+      if (event.target.className === 'modal') {
+        event.target.classList.add('hidden');
+      }
+    });
+  },
+  tableListener: function() {
+    var selectDay = document.querySelector('.week');
+
+    selectDay.addEventListener('click', function (event) {
+      var elementClicked = event.target;
+      if (elementClicked.className === 'week') {
+        return;
+      }
+      var day = elementClicked.innerText.split('\n\n')[0];
+      if (Number.isInteger(parseFloat(day))) {
+        return;
+      }
+      view.displayTodos(day);
+    });
   }
 };
 
-var selectDay = document.querySelector('.week');
-var today = document.querySelector('.today');
-var addEntry = document.querySelector('.addEntry');
-
-var modal = document.querySelector('.modal');
-var entryWeek = document.getElementById('entryWeek');
-var entryTime = document.getElementById('entryTime');
-var entryDescription = document.getElementById('entryDescription');
-var entrySubmit = document.getElementById('entrySubmit');
-
-var modalUpdate = document.querySelector('.modal-update');
-var updateWeek = document.getElementById('updateWeek');
-var updateTime = document.getElementById('updateTime');
-var updateDescription = document.getElementById('updateDescription');
-
-var tableBody = document.querySelector('tbody');
-
-addEntry.addEventListener('click', entryModal);
-selectDay.addEventListener('click', dayOfWeek);
-window.addEventListener('click', closeModal);
-entrySubmit.addEventListener('click', addTask);
-
-function entryModal() {
-  modal.classList.remove('hidden');
-}
-function closeModal(event) {
-  if (event.target === modal || event.target === modalUpdate) {
-    modal.classList.add('hidden');
-    modalUpdate.classList.add('hidden');
-  }
-}
-
-function dayOfWeek(event) {
-  if (event.target.className === 'week') {
-    return;
-  }
-  var sentence = event.target.textContent;
-  today.textContent = 'Scheduled Events for ' + sentence;
-
-  var todaySchedule = schedule[sentence].task;
-  tableBody.innerHTML = '';
-
-
-  todaySchedule.forEach(function(row, index) {
-    var tr = document.createElement('tr');
-    var tdTime = document.createElement('td');
-    var tdDesc = document.createElement('td');
-    tr.id = index;
-
-    tdTime.textContent = todaySchedule[index].time;
-    tdDesc.textContent = todaySchedule[index].description;
-
-    tr.append(tdTime, tdDesc);
-    tableBody.append(tr);
-  }, this);
-
-  // for (var i = 0; i < todaySchedule.length; i++) {
-  //   var tr = document.createElement('tr');
-  //   var tdTime = document.createElement('td');
-  //   var tdDesc = document.createElement('td');
-
-  //   var updateBtn = document.createElement('button');
-  //   updateBtn.className = 'update-button ' + i;
-  //   updateBtn.textContent = "Update";
-
-
-
-  // tdDesc.append(updateBtn);
-  //   tr.append(tdTime, tdDesc);
-  //   tableBody.append(tr);
-  // }
-
-  var updateButton = document.querySelector('tbody');
-  updateButton.addEventListener('click', updateModal);
-
-  function updateModal(event) {
-    if (event.target.className.indexOf('update-button') === -1) {
-      return;
-    }
-    modalUpdate.classList.remove('hidden');
-    updateWeek.value = sentence;
-    updateTime.value = schedule[sentence].task[event.target.classList[1]].time;
-    updateDescription.value = schedule[sentence].task[event.target.classList[1]].description;
-  }
-
-  var updateSubmit = document.getElementById('updateSubmit');
-  updateSubmit.addEventListener('click', updateTask);
-
-  function updateTask(event) {
-    event.preventDefault();
-
-    var newTask = {
-      day: updateWeek.value,
-      time: updateTime.value,
-      description: updateDescription.value,
-    };
-
-    schedule[updateWeek.value].task.push(newTask);
-
-    modalUpdate.classList.add('hidden');
-    updateWeek.value = '';
-    updateTime.value = '';
-    updateDescription.value = '';
-
-  }
-}
-
-function addTask(event) {
-  event.preventDefault();
-
-  var newTask = {
-    day: entryWeek.value,
-    time: entryTime.value,
-    description: entryDescription.value,
-  };
-
-  schedule[entryWeek.value].task.push(newTask);
-
-  modal.classList.add('hidden');
-  entryWeek.value = '';
-  entryTime.value = '';
-  entryDescription.value = '';
-}
+view.entryListener();
+view.buttonListener();
+view.modalListener();
+view.tableListener();
